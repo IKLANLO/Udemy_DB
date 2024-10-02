927c60be2db0ca9bb4d0827b7474872fc8361d4f
IKLANLO <ilandaberea@yahoo.es>
2024-10-02 21:32:48 +0200

    guardado de datos en instancia de capa entidad + validaciones en botones y cuadro de diálogo

---
+++ capaEntidad/CEEmpleado.vb
@@ -0,0 +1,48 @@
+﻿Public Class CEEmpleado
+
+    Private _id As Integer
+    Private _nombre As Integer
+    Private _apellido As Integer
+    Private _foto As Integer
+
+    'creamos el constructor sin ningún valor aún'
+    Public Sub New()
+    End Sub
+
+    'creamos getters y setters para las variables privadas'
+    Public Property Id As Integer
+        Get
+            Return _id
+        End Get
+        Set(value As Integer)
+            _id = value
+        End Set
+    End Property
+
+    Public Property Nombre As Integer
+        Get
+            Return _nombre
+        End Get
+        Set(value As Integer)
+            _nombre = value
+        End Set
+    End Property
+
+    Public Property Apellido As Integer
+        Get
+            Return _apellido
+        End Get
+        Set(value As Integer)
+            _apellido = value
+        End Set
+    End Property
+
+    Public Property Foto As Integer
+        Get
+            Return _foto
+        End Get
+        Set(value As Integer)
+            _foto = value
+        End Set
+    End Property
+End Class
--- capaPresentacion/frEmpleados.vb
+++ capaPresentacion/frEmpleados.vb
@@ -1,9 +1,50 @@
-﻿Public Class frEmpleados
-    Private Sub frEmpleados_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports capaEntidad
 
+Public Class frEmpleados
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+
+        'vaciamos el formulario'
+        txtNombre.Clear()
+        txtApellido.Clear()
+        txtId.Value = 0
+        picFoto.Image = Nothing
     End Sub
 
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+    Private Sub lnkFoto_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkFoto.LinkClicked
+        'indicamos los tipos de archivo admitidos en el cuadro de diálogo'
+        openFoto.Filter = "jpg files (*.jpg)|*.jpg|png files (*.png)|*.png"
 
+        'abrimos el cuadro'
+        openFoto.ShowDialog()
+
+        If openFoto.FileName IsNot "" Then
+            'ajustamos el tamaño de la foto al tamaño del componente picFoto'
+            picFoto.SizeMode = PictureBoxSizeMode.StretchImage
+
+            'si se ha seleccionado alguna foto la cargamos en picFoto'
+            picFoto.Load(openFoto.FileName)
+        End If
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        'creamos una instancia de CEEmpleado'
+        Dim empleado As New CEEmpleado()
+
+        'guardamos los datos del formulario en esa instancia'
+        empleado.Id = txtId.Value
+        empleado.Nombre = txtNombre.Text
+        empleado.Apellido = txtApellido.Text
+        empleado.Foto = picFoto.ImageLocation
+    End Sub
+
+    Private Sub TextBox_TextChanged(sender As Object, e As EventArgs) Handles MyBase.Load, txtNombre.TextChanged, txtApellido.TextChanged, txtId.TextChanged, picFoto.TextChanged
+        'controlamos que sólo se activen los botones de guardar y eliminar si están los campos rellenos'
+        If txtId.Value = 0 Or txtNombre.Text Is "" Or txtApellido.Text Is "" Or picFoto.Image Is "" Then
+            btnGuardar.Enabled = False
+            btnEliminar.Enabled = False
+        Else
+            btnGuardar.Enabled = True
+            btnEliminar.Enabled = True
+        End If
     End Sub
 End Class
