b68132fcfa83c46ea84dca22445e8f7dac1ecf0d
IKLANLO <ilandaberea@yahoo.es>
2024-10-03 12:22:02 +0200

    cambiado fondo de componentes y activación de botones según estén los campos rellenados o no

---
+++ capaNegocio/CNEmpleado.vb
@@ -0,0 +1,15 @@
+﻿Imports capaEntidad
+
+Public Class CNEmpleado
+    Public Function ValidarDatos(ByVal empleado As CEEmpleado) As Boolean
+        'devolverá true o false según se validen los datos del objeto CEEmpleado pasado'
+        Dim Resultado As Boolean = True
+
+        If empleado.Id = 0 Or empleado.Nombre = "" Or empleado.Apellido = "" Or empleado.Foto = "" Then
+            Resultado = False
+            MessageBox.Show("Todos los datos son obligatorios", "Error", MessageBoxButtons.OK)
+        End If
+
+        Return Resultado
+    End Function
+End Class
--- capaPresentacion/frEmpleados.vb
+++ capaPresentacion/frEmpleados.vb
@@ -1,6 +1,12 @@
-﻿Imports capaEntidad
+﻿Imports System.Runtime.CompilerServices
+Imports capaEntidad
+Imports capaNegocio
 
 Public Class frEmpleados
+    Dim NegocioEmpleado As New CNEmpleado
+    Dim redColor As Color = Color.FromArgb(238, 210, 206)
+    Dim whiteColor As Color = Color.White
+
     Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
 
         'vaciamos el formulario'
@@ -35,16 +41,50 @@ Public Class frEmpleados
         empleado.Nombre = txtNombre.Text
         empleado.Apellido = txtApellido.Text
         empleado.Foto = picFoto.ImageLocation
+
+        NegocioEmpleado.ValidarDatos(empleado)
+
     End Sub
 
-    Private Sub TextBox_TextChanged(sender As Object, e As EventArgs) Handles MyBase.Load, txtNombre.TextChanged, txtApellido.TextChanged, txtId.TextChanged, picFoto.TextChanged
+    Private Sub TextBox_TextChanged(sender As Object, e As EventArgs) Handles MyBase.Load, txtNombre.TextChanged, txtApellido.TextChanged, txtId.TextChanged, openFoto.FileOk
         'controlamos que sólo se activen los botones de guardar y eliminar si están los campos rellenos'
-        If txtId.Value = 0 Or txtNombre.Text Is "" Or txtApellido.Text Is "" Or picFoto.Image Is "" Then
+        If txtId.Value = 0 Or txtNombre.Text Is "" Or txtApellido.Text Is "" Or String.IsNullOrEmpty(openFoto.FileName) Then
             btnGuardar.Enabled = False
             btnEliminar.Enabled = False
+
+            ReviewBackColor()
         Else
             btnGuardar.Enabled = True
             btnEliminar.Enabled = True
+
+            ReviewBackColor()
         End If
     End Sub
+
+    Private Function ReviewBackColor()
+        'establecemos los valores de cada control en un array'
+        Dim contentReview() As String = {txtId.Value.ToString(), txtNombre.Text, txtApellido.Text, openFoto.FileName}
+
+        'establecemos un diccionario para identificar cada par valor:clave'
+        Dim controls As New Dictionary(Of String, Control) From {
+            {"txtId", txtId},
+            {"txtNombre", txtNombre},
+            {"txtApellido", txtApellido},
+            {"picFoto", picFoto}
+        }
+        'establecemos el color de fondo de cada control según si está rellenado o no'
+        Dim i As Integer = 0
+        For Each value As String In contentReview
+            If value Is "" Or value.ToString = "0" Then
+                If i < controls.Count Then
+                    controls.ElementAt(i).Value.BackColor = redColor
+                End If
+
+            ElseIf i < controls.Count Then
+                controls.ElementAt(i).Value.BackColor = whiteColor
+            End If
+
+            i += 1
+        Next
+    End Function
 End Class
